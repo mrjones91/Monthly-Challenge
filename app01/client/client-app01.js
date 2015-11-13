e71282f49686ded85b29678852b567d4aae2bdf3
dijdev <danieljonesdev@gmail.com>
2015-11-12 19:14:34 -0500

    last minute changes. post month

--- app01/client/client-app01.js
+++ app01/client/client-app01.js
@@ -34,7 +34,8 @@ if (Meteor.isClient) {
     },
 
     twitterUrl: function() {
-      return Session.get('url');
+      var twitUrl = Persons.findOne({username: $('#usn').val()})
+      return twitUrl.imageUrl;
     },
 
     testerUrl: function() {
@@ -62,45 +63,21 @@ if (Meteor.isClient) {
 
     'click #twitter': function() {
       var usn = $('#usn').val();
+      Persons.insert({
+        username: usn,
+        imageUrl: 'unset'
+      });
       //callStub(usn);
       console.log('Async Test');
       var username = { screen_name: usn};
       Meteor.call('test2', username, function(e, d){
         console.log('client result');
-        console.log(d);
+        console.log('E: ' + e);
+        console.log('D: ' + d);
+        
       });
-      //testCallAsync();
-      // Meteor.call('getURL', username, function(error, result){
-      //   alert('aysnc? ' + result);
-      //   console.log(result + ' jill');
-      //   Session.set('url', result);
-      //   alert(result);
-      // });
-
-      // var testUrl = Meteor.call('getURL', username);
-      // Session.set('url2', testUrl)
-      // alert('booty ' + testUrl);
+      
     }
     
   });
-
-  function callStub(usn){
-    var username = { screen_name: usn};
-    Meteor.call('getURL3', username, function(error, result){
-        //alert('woo');
-        console.log('res ' + result);
-        console.log('err ' + error);
-        console.log(result + ' client');
-        Session.set('url', result);
-        console.log(Session.get('url'));
-        //alert(result);
-      });
-  }
-
-  testCallAsync = function () {
-    Meteor.call('callAsync', function (err, res) {
-      if (err) console.log(err);
-      else console.log('response: ', res);
-    });
-  }
 }
--- app01/server/server-app01.js
+++ app01/server/server-app01.js
@@ -3,54 +3,16 @@ if (Meteor.isServer) {
     // code to run on server at startup
     
     console.log('Hello World!');
-  });
-
-  
-  var personId = Persons.insert({
-    name: 'Daniel Jones',
-    alias: 'DJ',
-    age: 24
-  });
-
+    var image_url;
+    Persons.remove({});
+  console.log('removed all Persons');
   Persons.insert({
-    name: 'John',
-    alias: 'John',
-    age: 33
+    username: 'test',
+    profile_image_url: 'http://test.test'
   });
 
-  Persons.update( { name: 'Daniel Jones'}, {$inc: {age: 35}} );
-
-  var processUrl = function(username){
-      var Twitter = Meteor.npmRequire('twitter-node-client').Twitter;
-      var config = {
-          'consumerKey': 'v3pJuN3hEB69F99A1aZL5AIbW',
-          'consumerSecret': 'WZV294rXidXvUciUyUu2blSXsv97BHiVvRkODd5qRKxwVXhkjl',
-          'accessToken': '117222781-y3Msu2uxmwAd1856REc7XIcPgicLcdRVu4Y3Idxd',
-          'accessTokenSecret': 'PhPUsJbaO7DB738tVte6ExVIGNKZy4iDjRpWKyNSl1ZYP',
-          'callBackUrl': 'XXX'
-      };
-      var twitter = new Twitter(config);
-
-      var image_url = 'no';
-
-      var error = function (err, response, body) {
-          console.log('ERROR [%s]', err);
-
-          return err;
-      };
-      var success = function (data) {
-          console.log('***************************************************');
-          //console.log('Data [%s]', data);
-
-          //console.log(data);
-          me = JSON.parse(data);
-          console.log('things' + me.profile_image_url);
-          image_url = me.profile_image_url;
-          //console.log('000\n' + image_url);
-          return image_url;
-      };
-      twitter.getUser({ screen_name: username} , error, success);
-  };
+ 
+  });
 
       var Twitter = Meteor.npmRequire('twitter-node-client').Twitter;
       var config = {
@@ -62,124 +24,72 @@ if (Meteor.isServer) {
       };
       var twitter = new Twitter(config);
 
-      var image_url = 'default';
-      var error = function (err, response, body) {
-          console.log('ERROR [%s]', err);
+      var boundError = Meteor.bindEnvironment(function(err){ 
+              console.log(err);
+              var errThing = {
+                username: 'error',
+                imageUrl: 'http://error.error'
+              };
+              console.log(errThing);
 
-          return err;
-      };
-      var success = function (data) {
-          console.log('***************************************************');
-          //console.log('Data [%s]', data);
+                console.log('bound');
+                Persons.insert(errThing);
+                var exists = Persons.find( {username: errThing.username });
+              if (exists) {
+                console.log('exists');
+                Persons.update( {username: errThing.username}, {$set:{imageUrl: errThing.imageUrl}} );
+                console.log('updated');
+              }
+              else {
+                console.log('no exists');
+                Persons.insert(errThing);
+                console.log('else updated');
+              }
+              
+              return errThing;
+            });
+      var boundSuccess = Meteor.bindEnvironment(function(suc) {
+              //get result and log it
+              console.log(suc);
+              console.log('log yea');
 
-          //console.log(data);
-          me = JSON.parse(data);
-          console.log('things ' + me.profile_image_url);
-          image_url = me.profile_image_url;
-          imageUrl = image_url;
-          console.log('global: ' + imageUrl);
-          //console.log('000\n' + image_url);
-          console.log(image_url);
-          return image_url;
-      };
-      var offlineErr = function() {
-        return 'offline error';
-      };
-      var offlineResult = function() {
-        return 'offline result';
-      };
-      var offlineMethod = function(data, offlineErr, offlineResult) {
-        console.log('data');
-        return 'offline call done';
-      }
+              //set to object
+              var thing = JSON.parse(suc);
+              var thingTwit = {
+                username: thing.screen_name,
+                imageUrl: thing.profile_image_url
+              }
+              
+              //make sure entry doesn't exist for same user
+              //if exists - update. else - insert
+              var exists = Persons.findOne( {username: thingTwit.username });
+              if (exists) {
+                console.log(exists.username + ' exists with ' + exists.imageUrl);
+                Persons.update( {username: thingTwit.username},  { $set: { imageUrl: thingTwit.imageUrl}} );
+                console.log(exists.username + ' updated');
+              }
+              else {
+                console.log('no exists');
+                Persons.insert({
+                  username: thingTwit.screen_name,
+                  imageUrl: thingieTwit.profile_image_url
 
-      function async(cb){
-        Meteor.setTimeout(function () {
-          cb(null, 'hello');
-        }, 3000);
-      }
-
+                });
+                console.log(thingTwit.username + ' inserted');
+              }
+              console.log('object based on data: ' + thingTwit.username + ' \n ' + thingTwit.imageUrl);
+              return thingTwit;
+      });
   
-//resolve the 2 meteor methods for peace in the server
       Meteor.methods({
         test2: function(data) {
-            data = Meteor.sync( function(done) {
-              console.log('started');
-              setTimeout(function(){
-                done(null, 2001);
-              }, 3000);
-              
-              //  function(err,res) {
 
-                 console.log('wat' + data.result);
-              //done(null, res);
-              // });
-          });
-          return data.result;
-        },
-        test: function() {
-          datasets = [];
-          datasets.push({ screen_name: 'dij_plz'});
-          datasets.push({ screen_name: 'deray'});
-          datasets.push({ screen_name: 'angryblacklady'});
-          datasets.push({ screen_name: 'the_mystery_one'});
-          var randomNum = Math.floor(Math.random() * datasets.length);
-          data = datasets[randomNum]
-          console.log(randomNum);
-          twitter.getUser(data, error, success);
-          return success;
-        },
-         callAsync: function () {
-      var fiber = Fiber.current;
- 
-      var fence = Meteor._CurrentWriteFence.get()
-        , handle = fence && fence.beginWrite();
- 
-      async(function (err, res) {
-        handle && handle.committed();
-        fiber.run(res);
-      });
- 
-      return Twitter.yield();
-    },
-        getURL3: function(username) {
-          console.log(error);
-          console.log(success);
-         
-          Meteor.wrapAsync(offlineMethod, this);
-          return image_url;
-         
-        },
-        'getURL2': function(username) {
-          //return processUrl(username);
-          var imageUrlSync = Meteor.wrapAsync(twitter.getUser, twitter);
-          imageUrlSync({
-            screen_name: username 
-          }, function (error, something) {
-            console.log('err ' + error);
-            console.log('some ' + something);
-          });
-      },
-      getURL4: function(username) {
-        //var getU1 = twitter.getUser(username , error, success);
-        var x = Meteor.wrapAsync(twitter.getUser, twitter);
+          var thingiething = twitter.getUser({screen_name: data.screen_name}, boundError, boundSuccess);
+          
 
-        var result = x(username);
-        console.log('idk ' + result);
-        return result;
-        //return getU1();
-        // var getU = Meteor.wrapAsync(twitter.getUser, twitter);
-        // getU({ screen_name: username}, function(err, result) {
-        //   if (err){
-        //     console.log('no works? ' +err);
-        //     return err;
-        //   }
-        //   else{
-        //     console.log(' works? ' + result);
-        //     return result;
-        //   }
-        // });
-      }
+         
+          return data.screen_name + ' inserted';
+        }
     });
 
 }//isServer
